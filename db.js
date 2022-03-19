359d819ebaa74e22c95928421618fc76b5cd1610
islambou9427 <islem.bouagada@gmail.com>
2022-03-19 18:37:38 +0100

    return  10 first  record

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 var unrest = require("unirest")
-var req = unrest ("GET", "http://localhost:3000/data")
+var req = unrest ("GET", "http://localhost:3000/data?_limit=10" )
 req.headers({
 "cache-control": "no-cache",
 })
+
 req. end(function (res){ 
 if (res.error) throw new Error(res.error)
 console.log(res.body)})
